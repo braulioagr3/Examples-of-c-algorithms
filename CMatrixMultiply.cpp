d0771ba6a210fa2019729326c5bfac42ff27d81a
Braulio A. Garcia Rivera <braulioagr3@gmail.com>
2021-04-21 18:03:06 -0500

    something

--- CMatrixMultiply.cpp
+++ CMatrixMultiply.cpp
@@ -6,6 +6,7 @@ void CMatrixMultiply::matrixMultiply()
     int columns;
     CMatrix* m1;
     CMatrix* m2;
+    CMatrix* ans;
     cout << "Please set de martix 1" << endl;
     cout << "Rows: ";
     cin >> rows;
@@ -22,7 +23,11 @@ void CMatrixMultiply::matrixMultiply()
     m2 = new CMatrix(rows,columns);
     m2->setValues();
     system("clear");
+    cout << "matrix 1:" << endl;
     m1->printValues();
+    cout << "matrix 2:" << endl;
     m2->printValues();
-
+    cout << "ans:" << endl;
+    ans= m1->multiply(m2);
+    ans->printValues();
 }
